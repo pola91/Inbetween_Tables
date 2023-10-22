60c23de9e783c1b6936c3f85bf194b50fb7b44a0
pola91 <pola_moses91@yahoo.com>
2023-10-22 13:24:20 +0300

    Update ViewController.swift

--- zenkit-kanban.git/InBetweenCollectionViewsDragAndDrop/InBetweenCollectionViewsDragAndDrop/ViewController.swift
+++ zenkit-kanban.git/InBetweenCollectionViewsDragAndDrop/InBetweenCollectionViewsDragAndDrop/ViewController.swift
@@ -2,9 +2,6 @@
 //  ViewController.swift
 //  InBetweenCollectionViewsDragAndDrop
 //
-//  Created by Jean Joseph on 9/13/17.
-//  Copyright © 2017 Jean Joseph. All rights reserved.
-//
 
 import UIKit
 
